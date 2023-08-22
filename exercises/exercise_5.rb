fac2940e61899a145bd382658e5f733d93935ba1
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-21 20:18:25 -0400

    completes ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,9 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
-# Your code goes here ...
+puts "Total revenue: $#{Store.sum(:annual_revenue)}"
+puts "Average revenue: $#{Store.average(:annual_revenue).round}"
+puts "Stores generating over $1 million: #{Store.where(annual_revenue: ..1_000_000).count}"
