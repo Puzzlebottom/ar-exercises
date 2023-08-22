c70c88bd54a0991868de85ac4bb87792736ec00f
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-22 10:29:19 -0400

    completes stretch ex 1

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,14 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts 'Exercise 8'
+puts '----------'
+
+@fat_travis = Store.find(4).employees.create(first_name: 'Fat', last_name: 'Travis', hourly_rate: 111)
+puts "Password: #{@fat_travis.password}"
--- lib/employee.rb
+++ lib/employee.rb
@@ -3,4 +3,13 @@ class Employee < ActiveRecord::Base
   validates_presence_of :store
   validates :store_id, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+  after_create { update('password': generate_random_string(8)) }
+
+  private
+
+  def generate_random_string(length)
+    o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+    (0...length).map { o[rand(o.length)] }.join
+  end
 end
